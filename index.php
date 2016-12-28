c7315d344e8b338e97cb13b66689ff2f2083d4e5
Unknown <adouardcamille@hotmail.com>
2016-12-28 16:53:48 +0100

    Setup css for project display

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
               <?php echo $donnees['contexte_proj_fiche']; ?>
             </strong>
           </h2>
-          <h3>Deescription :</h3>
+          <h3>Description :</h3>
           <h2>
             <strong>
               <?php echo $donnees['description_proj_fiche']; ?>
@@ -158,6 +158,145 @@
               <?php echo $donnees['perim_proj_fiche']; ?>
             </strong>
           </h2>
+        </div>
+        <div class="col-md-12">
+          <h1>
+            Déploiement
+          </h1>
+          <h3>Date envisagée de mise à disposition</h3>
+          <h2>
+            <strong>
+              <?php echo $donnees['date_deploi_fiche']; ?>
+            </strong>
+          </h2>
+          <h3>Ce qui paraît incontournable pour la réussite ?</h3>
+          <h2>
+            <strong>
+              <?php echo $donnees['facteur_succ_fiche']; ?>
+            </strong>
+          </h2>
+          <h3>Qui va porter cette offre au sein de nos réseaux</h3>
+          <h2>
+            <strong>
+              <?php echo $donnees['port_offre_fiche']; ?>
+            </strong>
+          </h2>
+          <div class="col-md-12">
+            <div class="col-md-4">
+              <h3>Canal de déploiement</h3>
+              <h2>
+                Ligne managériale
+                <strong>
+                  <?php echo $donnees['lignem_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Pédagogie en réunion de zone
+                <strong>
+                  <?php echo $donnees['pedagogie_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Rituels existants
+                <strong>
+                  <?php echo $donnees['rituelfiche_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Précisions
+                <strong>
+                  <?php
+                    echo $donnees['rituely_fiche'];
+                    echo $donnees['ritueln_fiche'];
+                  ?>
+                </strong>
+              </h2>
+            </div>
+            <div class="col-md-4">
+              <h3>Professionalisation</h3>
+              <h2>
+                Appropriation à prévoir
+                <strong>
+                  <?php echo $donnees['approp_fiche']; ?>
+                  <?php echo $donnees['mode_apropr_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Relais de professionalisation
+                <strong>
+                  <?php echo $donnees['relprof_fiche']; ?>
+                </strong>
+              </h2>
+            </div>
+            <div class="col-md-4">
+              <h3>Diffusion des supports</h3>
+              <h2>
+                Essentiel
+                <strong>
+                  <?php echo $donnees['diffess_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Intranet BU
+                <strong>
+                  <?php echo $donnees['diffintra_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Digital
+                <strong>
+                  <?php echo $donnees['diffdig_fiche']; ?>
+                </strong>
+              </h2>
+              <h2>
+                Autre
+                <strong>
+                  <?php echo $donnees['diffelse_fiche']; ?>
+                </strong>
+              </h2>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <h1>
+              Synthèse
+            </h1>
+            <h2>
+              Effort d'implémentation
+              <strong>
+                <?php echo $donnees['syntheff_fiche']; ?>
+              </strong>
+            </h2>
+            <h2>
+              Différenciation vs concurrence
+              <strong>
+                <?php echo $donnees['synthdiff_fiche']; ?>
+              </strong>
+            </h2>
+            <h2>
+              temps nécessaire déploiement
+              <strong>
+                <?php echo $donnees['synthtime_fiche']; ?>
+              </strong>
+            </h2>
+            <h2>
+              Facilité de mesure d'impact
+              <strong>
+                <?php echo $donnees['synthimp_fiche']; ?>
+              </strong>
+            </h2>
+            <h2>
+              Impact business
+              <strong>
+                <?php echo $donnees['synthcrea_fiche']; ?>
+              </strong>
+            </h2>
+            <h2>
+              Motivations personnelles
+              <strong>
+                <?php echo $donnees['synthmotiv_fiche']; ?>
+              </strong>
+            </h2>
+          </div>
         </div>
       </div>
     <?php
