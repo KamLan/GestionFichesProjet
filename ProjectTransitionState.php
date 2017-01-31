215c6575cc772fb915680130c7c3cde89216a8eb
Unknown <adouardcamille@hotmail.com>
2017-01-31 16:19:29 +0100

    Fonction mail fonctionnelle

--- ProjectTransitionState.php
+++ ProjectTransitionState.php
@@ -2,7 +2,10 @@
 include 'bdd.php';
 $ficheid = $_GET['ficheid'];
 $etatfiche = $_GET['etat'];
-
+$to = "";
+$subject = "";
+$headers = "";
+$message = "";
 
   if ($etatfiche == 'approved') {
 
@@ -27,6 +30,29 @@ $etatfiche = $_GET['etat'];
   else {
 
   }
+
+  $req_mail = $bdd->query('SELECT * FROM fiches WHERE id_fiche="'.$ficheid.'"');
+  while ($donnees4 = $req_mail->fetch())
+  {
+    $email_pil=$donnees4['pilote_email_fiche'];
+    $nom_pil=$donnees4['pilote_nom_fiche'];
+    $tel_pil=$donnees4['pilote_mobile_fiche'];
+    $titre_proj=$donnees4['titre_projet'];
+
+    $to = $email_pil;
+    $subject .= 'Votre projet a changé de statut!';
+    $headers .= 'Content-Type: text/html; charset=\"iso-8859-1\"';
+    $headers .= 'From: "adouardcamille.com"';
+    $headers .= 'Reply-To: adouardcamille@hotmail.com';
+    $message .= '<h2> Bonjour !<h2>';
+    $message .= '<h3> Le statut du projet ci-dessous a été modifié: </h3>';
+    $message .= '<h4>'.$titre_proj.'</h4>';
+    $message .= '<h3> Statut : </h3>';
+    $message .= '<h4>'.$donnees4['validation_fiche'].'</h4>';
+  }
+  $req_mail->closeCursor();
+
+  mail($to, $subject, $message, $headers);
 
 
  ?>
--- updateComments.php
+++ updateComments.php
@@ -1,32 +1,23 @@
 <?php
 include 'bdd.php';
 $ficheid = $_GET['id'];
-$email_pil="";
-
-    $req_mail = $bdd->query('SELECT commentaire_fiche FROM fiches WHERE id_fiche="'.$ficheid.'"');
-    while ($donnees4 = $req_mail->fetch())
-    {
-      $email_pil=$_donnees4['email_pil'];
-      $nom_pil=$_donnees4['nom_pil'];
-      $tel_pil=$_donnees4['tel_pil'];
-      $titre_proj=$_donnees4['titre_proj'];
-
-      $to = $email_pil;
-      $subject = 'Un commentaire a été ajouté sur votre projet!';
-      $headers = 'Content-Type: text/html; charset=\"iso-8859-1\"';
-      $message = '<p> Bonjour ! <p>';
-      $message .= '<p> un nouveau commentaire a été ajouté sur votre projet: </p>';
-      $message .= '<p>'.$titre_proj.'</p>';
-
-      mail($to, $subject, $message, $headers);
-    }
-    $req_mail->closeCursor();
-
-      $to3 = 'adouardcamille@hotmail.com';
-      $subject3 = 'Hello';
-      $message3 = 'testytest';
-
-      mail($to3, $subject3, $message3);
+$email_pil= "";
+$to = "";
+$subject = "";
+$headers = "";
+$message = "";
+$commentaires = "";
+$lastcomment = $_POST['comm_proj'];
+      // $to3 = 'adouardcamille@hotmail.com';
+      // $subject3 = 'Hello';
+      // $message3 = '<h1> Hello ma friend ! </h1>';
+      // $message3 .= '<p> This is a <u>test message</u></p>';
+      // $message3 .= '<p> If you receive me you did succeed !!<p>';
+      // $headers3 = 'Content-Type: text/html; charset=\"iso-8859\"';
+      // $headers .= 'From: "adouardcamille.com"';
+      // $headers .= 'Reply-To: adouardcamille@hotmail.com';
+      //
+      // mail($to3, $subject3, $message3, $headers3);
 
     $req_ap = $bdd->query('SELECT commentaire_fiche FROM fiches WHERE id_fiche="'.$ficheid.'"');
     while ($donnees3 = $req_ap->fetch())
@@ -40,6 +31,30 @@ $email_pil="";
       'comm_proj' => $commentaires,
       'id' => $ficheid,
       ));
+
+      $req_mail = $bdd->query('SELECT * FROM fiches WHERE id_fiche="'.$ficheid.'"');
+      while ($donnees4 = $req_mail->fetch())
+      {
+        $email_pil=$donnees4['pilote_email_fiche'];
+        $nom_pil=$donnees4['pilote_nom_fiche'];
+        $tel_pil=$donnees4['pilote_mobile_fiche'];
+        $titre_proj=$donnees4['titre_projet'];
+
+        $to = $email_pil;
+        $subject .= 'Un commentaire a été ajouté sur votre projet!';
+        $headers .= 'Content-Type: text/html; charset=\"iso-8859-1\"';
+        $headers .= 'From: "adouardcamille.com"';
+        $headers .= 'Reply-To: adouardcamille@hotmail.com';
+        $message .= '<h2> Bonjour !<h2>';
+        $message .= '<h3> un nouveau commentaire a été ajouté sur votre projet: </h3>';
+        $message .= '<h4>'.$titre_proj.'</h4>';
+        $message .= '<h3> commentaire : </h3>';
+        $message .= '<h4>'.$lastcomment.'</h4>';
+      }
+      $req_mail->closeCursor();
+
+      mail($to, $subject, $message, $headers);
+
 
     header('location:index.php');
 
