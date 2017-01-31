2cf4a050594d6c4def36ce5d03e2165a5dc0a2a8
Unknown <adouardcamille@hotmail.com>
2017-01-31 11:49:11 +0100

    Mail function for comments

--- updateComments.php
+++ updateComments.php
@@ -1,7 +1,32 @@
 <?php
 include 'bdd.php';
 $ficheid = $_GET['id'];
+$email_pil="";
 
+    $req_mail = $bdd->query('SELECT commentaire_fiche FROM fiches WHERE id_fiche="'.$ficheid.'"');
+    while ($donnees4 = $req_mail->fetch())
+    {
+      $email_pil=$_donnees4['email_pil'];
+      $nom_pil=$_donnees4['nom_pil'];
+      $tel_pil=$_donnees4['tel_pil'];
+      $titre_proj=$_donnees4['titre_proj'];
+
+      $to = $email_pil;
+      $subject = 'Un commentaire a été ajouté sur votre projet!';
+      $headers = 'Content-Type: text/html; charset=\"iso-8859-1\"';
+      $message = '<p> Bonjour ! <p>';
+      $message .= '<p> un nouveau commentaire a été ajouté sur votre projet: </p>';
+      $message .= '<p>'.$titre_proj.'</p>';
+
+      mail($to, $subject, $message, $headers);
+    }
+    $req_mail->closeCursor();
+
+      $to3 = 'adouardcamille@hotmail.com';
+      $subject3 = 'Hello';
+      $message3 = 'testytest';
+
+      mail($to3, $subject3, $message3);
 
     $req_ap = $bdd->query('SELECT commentaire_fiche FROM fiches WHERE id_fiche="'.$ficheid.'"');
     while ($donnees3 = $req_ap->fetch())
