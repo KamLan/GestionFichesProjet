fbdc3f20a8dc959f456e1f02f74db30ce2f21885
kam <adouardcamille@hotmail.com>
2017-03-31 08:18:05 +0200

    test commit

--- updateComments.php
+++ updateComments.php
@@ -29,17 +29,19 @@ $lastcomment = $_POST['comm_proj'];
         $nom_pil=$donnees4['pilote_nom_fiche'];
         $tel_pil=$donnees4['pilote_mobile_fiche'];
         $titre_proj=$donnees4['titre_projet'];
+        $id_projet=$donnees4['id_fiche'];
 
         $to = $email_pil;
         $subject .= 'Un commentaire a été ajouté sur votre projet!';
         $headers .= 'Content-Type: text/html; charset=\"iso-8859-1\"';
         $headers .= 'From: "adouardcamille.com"';
         $headers .= 'Reply-To: adouardcamille@hotmail.com';
-        $message .= '<h2> Bonjour !<h2>';
+        $message .= '<html><body><h2> Bonjour !<h2>';
         $message .= '<h3> un nouveau commentaire a été ajouté sur votre projet: </h3>';
         $message .= '<h4>'.$titre_proj.'</h4>';
         $message .= '<h3> commentaire : </h3>';
         $message .= '<h4>'.$lastcomment.'</h4>';
+        $message .= '<p> Lien vers le projet: http://www.camilleadouard/FichesProjetAdecco/displayfiche.php?ficheid='.$id_projet.' </p><body></html>';
       }
       $req_mail->closeCursor();
 
